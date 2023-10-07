a173fef1be06915493f8642abc443c364b01c250
Pedro-Chaves2505 <107230091+Pedro-Chaves2505@users.noreply.github.com>
2023-10-07 19:10:56 -0300

    Codigo funcional, mas sem as funções de tratamento de erro

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*tem alguma forma de fazer o programa parar estando dentro de uma outra função que não a main? posso fazer goto.  Tem alguma espécie de throw error*/
-
-/*
-Vou tentar colocar o tamanho pra ser um limitador e se chegarmos ao limite, o código terminou com êxito.
-*/
 
 int q0(char* str, int pos, int tam);
 
@@ -13,21 +8,13 @@ int q1(char* str, int pos, int tam);
   
 int q2(char* str, int pos, int tam);
 
-//Talvez seria melhor preencher com \000
 void preencheComBarraN(char* str, int tam);
 
-//depois verificar o que aconteceria se colocasse como void,
-//e a relação com o if
+
 int verificaSeChegouAoFinal(int tam, int pos);
 
 int main() {
-  
-/*
-É importante que não haja lixo de memória. Por isso, preencher com '\n'.
-Por exemplo, se colocarmos "A78", mas nas posições 3 a 5 tiver esse lixo
-de memória: "A&*", vai dizer que não pertence à linguagem. Porém, acho
-que o limitador de tamanho já cuida desse trabalho.
-*/
+
   char str[8] = {};
   
   preencheComBarraN(str, 8);
@@ -37,7 +24,6 @@ que o limitador de tamanho já cuida desse trabalho.
 int tam_str = strlen(str);
   
   if(0 <= tam_str && tam_str<=8){
-    //nada vai recebrr o retorno dessa função? Como efetivar que o codigo funciona?
     q0(str, 0, tam_str);
         
   }
@@ -60,7 +46,6 @@ int q0(char* str, int pos, int tam)
    q1(str, pos, tam);    
   }
   else{
-    //Verificar se precisa colocar pos++ aqui
     printf("ERRO: Q0 <= str[%d] = %c. A primeira posicao deve ser uma letra maiuscula\n", pos, str[pos]);
   }
 }
@@ -80,28 +65,12 @@ int q1(char* str, int pos, int tam){
   }
 }
 
-//Ver se posso chamar q2 dentro de 
-//q2. se não, posso criar um q3
-// igual a q2 e fazer com que um 
-//chame o outro
-/* O que vai dizer wue não tem mais
-de 8 símbolos vai ser o strlen.
-mas, posso transformar isso em um estado?*/
-
-/*Penso que depois de q1, a verificação vai ser  mesma.*/ 
 
 int q2(char* str, int pos, int tam){
   int eAlfabeticoMai = str[pos] >= 'A' && str[pos] <= 'Z';
   int eAlfabeticoMin = str[pos] >= 'a' && str[pos] <= 'z';
   int eNumerico = str[pos] >= '0' && str[pos] <= '9';
   int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
-/*Aqui havia um problema, pois
-vai rodar o if ainda que
-o verificaSeChegouAoFinal() for verdadeiro.
-Posso colocar dentro de um if, ainda que seja estranho. 
-mas, pelo menos vai resolver esse problema
-e se for uma inconsistência, penso ser uma inconsistência menor
-*/
 
   if(verificaSeChegouAoFinal(tam, pos));
 
@@ -121,10 +90,6 @@ int verificaSeChegouAoFinal(int pos, int tam){
   if(pos == tam){
     //lidar com o êxito
     
-    /*esse return está sendo posto só por uma questão sintática, pois 
-    mesmo que iremos aqui já direcionar para o acerto não chegando a processar
-    o
-    estou colocando esa função em um if*/
     printf("Sucesso\n");
     return 1;
   }
@@ -140,26 +105,3 @@ void preencheComBarraN(char* str, int tam){
   }
 }
 
-/*
-Posso fazer certos numeros wue se referem a tipos de erros e tratá-los na saída do q0
-
-o problema é que não é um tratamento imediato, e vejo isso como problemático.
-
-de toda forma, teria um switch case para cada tipo de erro qie foi retornado ao final de q0
-    
-*/
-
-/*
-Estou achando problemática essa forma 
-usando o tamanho para verificar se já chegou ao final
-porque parece que esse tamanho está sendo passado para vários escopos.
-pensei que talvez fosse um problema com relação à memória,
-mas acho que seria um problema
-mais com relação à inconsistência
-nessas passagens entre escopos.
-*/
-
-/*
-O que queria fazer era sair de todas essas funções
-aninhadas assim que chegasse ao final, assim, isso não seria um problema. mas é uma inconsistência que pode gerar um problema.
-*/
