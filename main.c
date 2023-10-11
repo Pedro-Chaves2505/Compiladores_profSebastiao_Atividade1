d8e90eb8404c64fbe9ba40d1e48e666b090db5e3
Pedro-Chaves2505 <107230091+Pedro-Chaves2505@users.noreply.github.com>
2023-10-10 23:11:29 -0300

    Adequação para ser conforme o AFN, e tirando o loop perguntando se deseja continuar, pois no meu não funcionou

--- main.c
+++ main.c
@@ -10,11 +10,21 @@ int q1(char* str, int pos, int tam);
   
 int q2(char* str, int pos, int tam);
 
+int q3(char* str, int pos, int tam);
+
+int q4(char* str, int pos, int tam);
+
+int q5(char* str, int pos, int tam);
+
+int q6(char* str, int pos, int tam);
+
+int q7(char* str, int pos, int tam);
+
 void preencheComBarraN(char* str, int tam);
 
 void tratementoError(int error, char* str, int pos);
 
-int verificaSeChegouAoFinal(int tam, int pos);
+int verificaSeChegouAoFinal(int pos, int tam, char* str);
 
 int main() {
   system("clear");
@@ -24,39 +34,26 @@ int main() {
   
   preencheComBarraN(str, 8);
 
-  char resp = 's';
-  while (resp == 's')
-  {
-    printf("Digite a cadeia a ser testada pelo autômato: ");
-    scanf("%s", &str);
+  printf("Digite a cadeia a ser testada pelo autômato: ");
+  scanf("%s", &str);
 
-    int tam_str = strlen(str);
-  
-    if(0 <= tam_str && tam_str<=8){
-      q0(str, 0, tam_str);   
-    }
-    else{
-      //erro
-      tratementoError(3, str, 0);
-      return 1;
-    }
+  int tam_str = strlen(str);
 
-
-    printf("\nDigite S para continuar ou N para encerrar: ");
-    fflush(stdin);
-    scanf("%c", &resp);
-
-    resp = tolower(resp);
+  if(0 <= tam_str && tam_str<=8){
+    q0(str, 0, tam_str);   
+  }
+  else{
+    //erro
+    tratementoError(3, str, 0);
+    return 1;
   }
   
-  
-
   return 0;
 }
 
 int q0(char* str, int pos, int tam)
 {
-  if(verificaSeChegouAoFinal(pos, tam)){
+  if(verificaSeChegouAoFinal(tam, pos, str)){
   }
   else if(str[pos] >= 'A' && str[pos] <= 'Z'){
    printf("Q0 <= str[%d] = '%c'\n", pos, str[pos]);
@@ -70,7 +67,7 @@ int q0(char* str, int pos, int tam)
 }
 
 int q1(char* str, int pos, int tam){
-  if(verificaSeChegouAoFinal(pos, tam)){
+  if(verificaSeChegouAoFinal(tam, pos, str)){
   }
 
   else if(str[pos] >= '0' && str[pos] <= '9'){
@@ -91,25 +88,125 @@ int q2(char* str, int pos, int tam){
   int eNumerico = str[pos] >= '0' && str[pos] <= '9';
   int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
 
-  if(verificaSeChegouAoFinal(tam, pos));
+  if(verificaSeChegouAoFinal(tam, pos, str));
 
   else if(eAlfabeticoMai || eAlfabeticoMin || eNumerico || eEspecialPermit){
     
     //Colocar a informação da posição e que é Alfanumérico ou pertencente aos simbolos especiais permitidos
     printf("Q2 <= str[%d] = '%c'\n", pos, str[pos]);
     pos++;
-    q2(str, pos, tam);
+    q3(str, pos, tam);
   }
   else{
     tratementoError(2, str, pos);
   }
 }
 
-int verificaSeChegouAoFinal(int pos, int tam){
+int q3(char* str, int pos, int tam){
+  int eAlfabeticoMai = str[pos] >= 'A' && str[pos] <= 'Z';
+  int eAlfabeticoMin = str[pos] >= 'a' && str[pos] <= 'z';
+  int eNumerico = str[pos] >= '0' && str[pos] <= '9';
+  int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
+
+  if(verificaSeChegouAoFinal(tam, pos, str));
+
+  else if(eAlfabeticoMai || eAlfabeticoMin || eNumerico || eEspecialPermit){
+    
+    //Colocar a informação da posição e que é Alfanumérico ou pertencente aos simbolos especiais permitidos
+    printf("Q3 <= str[%d] = '%c'\n", pos, str[pos]);
+    pos++;
+    q4(str, pos, tam);
+  }
+  else{
+    tratementoError(2, str, pos);
+  }
+}
+
+int q4(char* str, int pos, int tam){
+  int eAlfabeticoMai = str[pos] >= 'A' && str[pos] <= 'Z';
+  int eAlfabeticoMin = str[pos] >= 'a' && str[pos] <= 'z';
+  int eNumerico = str[pos] >= '0' && str[pos] <= '9';
+  int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
+
+  if(verificaSeChegouAoFinal(tam, pos, str));
+
+  else if(eAlfabeticoMai || eAlfabeticoMin || eNumerico || eEspecialPermit){
+    
+    //Colocar a informação da posição e que é Alfanumérico ou pertencente aos simbolos especiais permitidos
+    printf("Q4 <= str[%d] = '%c'\n", pos, str[pos]);
+    pos++;
+    q5(str, pos, tam);
+  }
+  else{
+    tratementoError(2, str, pos);
+  }
+}
+
+int q5(char* str, int pos, int tam){
+  int eAlfabeticoMai = str[pos] >= 'A' && str[pos] <= 'Z';
+  int eAlfabeticoMin = str[pos] >= 'a' && str[pos] <= 'z';
+  int eNumerico = str[pos] >= '0' && str[pos] <= '9';
+  int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
+
+  if(verificaSeChegouAoFinal(tam, pos, str));
+
+  else if(eAlfabeticoMai || eAlfabeticoMin || eNumerico || eEspecialPermit){
+    
+    //Colocar a informação da posição e que é Alfanumérico ou pertencente aos simbolos especiais permitidos
+    printf("Q5 <= str[%d] = '%c'\n", pos, str[pos]);
+    pos++;
+    q6(str, pos, tam);
+  }
+  else{
+    tratementoError(2, str, pos);
+  }
+}
+
+int q6(char* str, int pos, int tam){
+  int eAlfabeticoMai = str[pos] >= 'A' && str[pos] <= 'Z';
+  int eAlfabeticoMin = str[pos] >= 'a' && str[pos] <= 'z';
+  int eNumerico = str[pos] >= '0' && str[pos] <= '9';
+  int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
+
+  if(verificaSeChegouAoFinal(tam, pos, str));
+
+  else if(eAlfabeticoMai || eAlfabeticoMin || eNumerico || eEspecialPermit){
+    
+    //Colocar a informação da posição e que é Alfanumérico ou pertencente aos simbolos especiais permitidos
+    printf("Q6 <= str[%d] = '%c'\n", pos, str[pos]);
+    pos++;
+    q7(str, pos, tam);
+  }
+  else{
+    tratementoError(2, str, pos);
+  }
+}
+
+int q7(char* str, int pos, int tam){
+  int eAlfabeticoMai = str[pos] >= 'A' && str[pos] <= 'Z';
+  int eAlfabeticoMin = str[pos] >= 'a' && str[pos] <= 'z';
+  int eNumerico = str[pos] >= '0' && str[pos] <= '9';
+  int eEspecialPermit = ( str[pos] == '@' || str[pos] == '*' || str[pos] == '_' || str[pos] == '-' || str[pos] == '#');
+
+  if(eAlfabeticoMai || eAlfabeticoMin || eNumerico || eEspecialPermit){
+    
+    //Colocar a informação da posição e que é Alfanumérico ou pertencente aos simbolos especiais permitidos
+    printf("Q7 <= str[%d] = '%c'\n", pos, str[pos]);
+    printf("A cadeia %s pertence a linguagem!\n", str);
+    
+  }
+
+  else{
+    tratementoError(2, str, pos);
+  }
+}
+
+int verificaSeChegouAoFinal(int pos, int tam, char* str){
+
   if(pos == tam){
     //lidar com o êxito
     
-    printf("Sucesso\n");
+    printf("A cadeia %s pertence a linguagem!\n", str);
     return 1;
   }
   else{
@@ -136,7 +233,7 @@ void tratementoError(int error, char* str, int pos){
     break;
 
   case 2:
-    printf("ERRO: Q2 <= str[%d] = '%c'. Da terceira posicao em diante deve ser uma letra do alfabeto, um algarismo ou um dos simbolos '@', '*', '_', '-', '#'\n", pos, str[pos]);
+    printf("ERRO: Q%d <= str[%d] = '%c'. Da terceira posicao em diante deve ser uma letra do alfabeto, um algarismo ou um dos simbolos '@', '*', '_', '-', '#'\n", pos, pos, str[pos]);
     break;
 
   case 3:
